158e0e7ce8f3bb04e46d487e5fd2e127a0ab53b4
nwb8 <nwb8@fresnou.org>
2023-02-06 10:02:21 -0800

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,96 @@
+// Create a variable with let
+let myVariable = "value";
+let myNewVariable = 7;
+let myBool = true;
+
+
+
+// Accessing variable values
+console.log(myVariable);
+console.log(myNewVariable + 5);
+console.log(myBool && true);
+
+
+
+//Update value stored in variable
+myNewVariable = 10;
+console.log(myNewVariable);
+
+
+
+// Increase by 1
+myNewVariable = myNewVariable + 1;
+console.log(myNewVariable);
+
+
+
+// Decrease by 1
+myNewVariable = myNewVariable - 1;
+console.log(myNewVariable);
+
+
+
+// Multiply by 3
+myNewVariable = myNewVariable * 3;
+console.log(myNewVariable);
+
+
+
+// Divide by 3
+myNewVariable = myNewVariable / 3;
+console.log(myNewVariable);
+
+
+
+// Divide and return the remainder
+myNewVariable = myNewVariable % 2; // 10/2 = 5 with 0 remainder
+console.log(myNewVariable);
+
+
+
+// Shortcuts!
+let x = 100;
+x = x+1; //add 1
+x++; //add 1
+
+let y=100;
+y=y-1 //minus 1
+y--; //minus 1
+let z = 20;
+z = z+5 //add 5
+
+let answer = 45;
+answer = answer -2; //subtract 2
+answer -=2; //subtract 2
+
+//other shortcuts
+answer *= 7; //multiply by 7
+answer /= 2; //divide by 2
+
+//keywords
+const pi = 3.14;
+const username = "SUSHIEATERONTHEROOF";
+//constants cannot change values guys (rizz)
+
+var old =123;
+//var is the old way to create variables instead of let.
+//Dont ever use var because it'll break easily.
+
+x = x - 1; //subtract 1
+x--; //subtract 1
+x = x + 5; //add 5
+x+= 5; //add 5
+x = x - 2; //subtract 2
+x -= 2; //subtract 2
+x = x * 10; //times 10
+x *= 10; //times 10
+x = x / 10; //divide by 10
+x /= 10; //divide by 10
+//idk
+x = 10;
+x = x + 1;
+x = x + 2;
+x = x * 3;
+x = x / 9;
+x = x - 1;
+console.log(z = z % 10);
