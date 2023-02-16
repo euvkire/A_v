d172a4a76df7c9bee8d1faa0d86ba919bf1be5c5
nwb8 <nwb8@fresnou.org>
2023-02-16 10:28:32 -0800

    updated inputstwo

--- inputs2.js
+++ inputs2.js
@@ -48,27 +48,26 @@ else {
 }
 //4
 let number3 = readline.question( "Enter a number :)" );
-if (number3 === 5)
-{ console.log ("Congrats You Won!!!");}
+if (number3 == 5)
+{ console.log ("Congrats You Won!!!")}
 
-if (number3>5)
-{console.log("Greater than Number");}
+ else if (number3 > 5)
+{console.log("Greater than Number")}
 
-if (number3<5)
-{ console.log("Less than Number" );}
+ else if (number3 < 5)
+{ console.log("Less than Number" )}
+ else {
+     console.log("Invalid");
+}
 //5
-let originalCost = readline.question( "Enter original price: ");
-let currentCost = readline.question( "Enter current price: ");
-let tax = originalCost*0.08;
-let originalCost = currentCost+tax;
-let discount = (currentCost / originalCost).toFixed(2);
-console.log(`discount: ${discount}`);
-//5 practice
-let cost = readline.question( "Enter a number");
-let tax = cost*0.08;
-let answer = cost+tax;
-console.log(`answer: $ {answer: ${})
-//6
+let cost = Number(readline.question("Enter meal cost: "));
+ let tax = cost * .08;
+ let newCost =  cost + tax
+console.log(`Your cost is ${newCost.toFixed( 2)}`);
+
+
+
+
 
 
 
---
+++ inputstwo.js
@@ -0,0 +1,35 @@
+//1
+const readline = require("readline-sync");
+let number = Number(readline.question("Enter a number: "));
+let number1 = Number(readline.question("Enter a number: "));
+let newSum = number + number1;
+console.log(`Your sum is ${newSum.toFixed( 2)}`);
+//2
+let number2 = Number(readline.question("Enter a number: "));
+let number3 = Number(readline.question("Enter a number: "));
+let newDifference = number2 - number3;
+console.log(`Your difference is ${newDifference.toFixed( 2)}`);
+//3
+let number4 = Number(readline.question("Enter a number: "));
+let number5 = Number(readline.question("Enter a number: "));
+let newProduct = number4 * number5;
+console.log(`Your product is ${newProduct.toFixed( 2)}`);
+//4
+let number6 = Number(readline.question("Enter a number: "));
+let number7 = Number(readline.question("Enter a number: "));
+let newQuotient = number6 / number7;
+console.log(`Your quotient is ${newQuotient.toFixed( 2)}`);
+//5
+let number8 = Number(readline.question("Enter a number: "));
+let number9 = Number(readline.question("Enter a number: "));
+let newRemainder = number8 % number9;
+console.log(`Your remainder is ${newRemainder.toFixed( 2)}`);
+//6
+let name1 = readline.question( "Enter your First name: " );
+let name2 = readline.question( "Enter your Last name: " );
+
+
+
+
+
+
