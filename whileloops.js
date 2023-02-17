0f04ac4abbd147914bb7c6b77487a82d1ecf7cf1
nwb8 <nwb8@fresnou.org>
2023-02-17 10:27:50 -0800

    updated while loops is crazy

---
+++ whileloops.js
@@ -0,0 +1,37 @@
+ const readline = require('readline-sync');
+//1
+// let a = 0;
+// while (a<26){
+//     console.log(a);
+//     a++;
+// }
+
+//2
+// let b = 0;
+// while (b<51){
+//     console.log(b);
+//     b++;
+// }
+
+//3
+// let c = -10;
+// while (c<11){
+//     console.log(c);
+//     c++;
+// }
+
+//4
+// let d = 0;
+// while (d>-51){
+//     console.log(d);
+//     d--;
+// }
+
+//5
+let e = 2;
+    while (e>102){
+        console.log(e);
+        e++;
+    }
+
+
