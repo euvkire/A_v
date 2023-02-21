a3cf71f271a47e928a1c3ea451e780fe675a9aad
nwb8 <nwb8@fresnou.org>
2023-02-21 09:58:06 -0800

    updated while loops is crazy

--- whileloops.js
+++ whileloops.js
@@ -28,10 +28,22 @@
 // }
 
 //5
-let e = 2;
-    while (e>102){
-        console.log(e);
-        e++;
-    }
+// let e = 2;
+//     while (e>102){
+//         console.log(e);
+//         e++;
+//     }
+//  let answer = readline.question("Enter y or n: ");
+//  while (answer != "y" && answer != "n"){
+//      console.log("invalid choice");
+//      answer = readline.question("Try again... Enter y or n: ")
+//  }
+//  console.log(`you typed ${answer}`);
 
+ let answer = readline.question("If you want to quit the program.. Enter y or n: ");
+ while (answer != "y" && answer != "n"){
+     console.log("invalid choice");
+     answer = readline.question("Try again.. If you want to quit the program.. Enter y or n:  ")
+ }
+ console.log(`you typed ${answer}`);
 
