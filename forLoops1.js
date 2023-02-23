84369527fb99748cb4be64c22526955b774f85d9
nwb8 <nwb8@fresnou.org>
2023-02-23 10:28:43 -0800

    updated while loops is crazy

---
+++ forLoops1.js
@@ -0,0 +1,28 @@
+//1
+for (let i = 0; i <= 250; i++){
+    console.log(i);
+}
+
+//2
+for (let i = 1; i <= 40; i++){
+        console.log(i*i);
+}
+
+//3
+for (let i = 1; i <= 30; i++){
+    console.log(i**3);
+}
+
+//4
+for(let i = 0; i<= 700; i++) {
+    if (i % 5 == 0) {
+        console.log(i)
+    }
+}
+
+//5
+for (let i = 350; i <= 1000; i++){
+    if(i % 10 == 0) {
+        console.log(i);
+    }
+}
