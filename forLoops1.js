e65faa5e351d6b715af892abd9c63d2707cb05a2
nwb8 <nwb8@fresnou.org>
2023-02-24 10:26:54 -0800

    updated while loops is crazy

--- forLoops1.js
+++ forLoops1.js
@@ -1,28 +1,74 @@
+const readline = require('readline-sync');
 //1
-for (let i = 0; i <= 250; i++){
-    console.log(i);
-}
+// for (let i = 0; i <= 250; i++){
+//     console.log(i);
+// }
 
 //2
-for (let i = 1; i <= 40; i++){
-        console.log(i*i);
-}
+// for (let i = 1; i <= 40; i++){
+//         console.log(i*i);
+// }
 
 //3
-for (let i = 1; i <= 30; i++){
-    console.log(i**3);
-}
+// for (let i = 1; i <= 30; i++){
+//     console.log(i**3);
+// }
 
 //4
-for(let i = 0; i<= 700; i++) {
-    if (i % 5 == 0) {
-        console.log(i)
-    }
-}
+// for(let i = 0; i<= 700; i++) {
+//     if (i % 5 == 0) {
+//         console.log(i)
+//     }
+// }
 
 //5
-for (let i = 350; i <= 1000; i++){
-    if(i % 10 == 0) {
-        console.log(i);
+// for (let i = 350; i <= 1000; i++){
+//     if(i % 10 == 0) {
+//         console.log(i);
+//     }
+// }
+
+//6
+// let sum = 0;
+// for (let i = 0; i <= 50; i++){
+//     sum+= i;
+// }
+// console.log(sum);
+
+//7
+// let sum = 0;
+// for (let i = 35; i <= 70; i++){
+//     sum+= i;
+// }
+// console.log(sum);
+
+//8
+// let sum = 1;
+// for (let i = 1; i <= 15; i++){
+//     sum*= i;
+// }
+// console.log(sum);
+
+//9
+// let sum = 1;
+// for (let i = 10; i <= 35; i++){
+//     sum*= i;
+// }
+// console.log(sum);
+
+//10 and 11 together
+let number= Number(readline.question("Enter a number: "));
+if (number * i ){
+    for(let i = 0; i<=12; i++)
+        console.log("Happy Holidays!");
+}
+
+let sum = 1;
+for (let i = 1; i <= 100; i++){
+    if (i % 2 == 0) {
+        sum += i;
     }
 }
+console.log(`sum: ${sum}`);
+
+
