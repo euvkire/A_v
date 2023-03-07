b34efec5ae56e030f8076256c200e4af18aa34a2
nwb8 <nwb8@fresnou.org>
2023-03-07 10:29:25 -0800

    updated while loops is crazy

---
+++ FizzBuzz.js
@@ -0,0 +1,14 @@
+for(let a = 0; a<= 100; a++) {
+    if (a % 3 == 0)
+    {
+        console.log('Fizz');
+
+    }
+
+    if (a % 5 == 0) {
+        console.log('Buzz');
+    } if (a % 15 == 0) {
+        console.log('FizzBuzz');
+    }if (a % 1 == 0)
+    {        console.log(a)}
+}
